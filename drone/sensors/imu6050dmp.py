945d63af7b532de93d6ecda29ade9aca8774701c
dpm76 <davidpm.itengineer@gmail.com>
2016-05-16 10:31:13 +0200

    Calibration angles file

--- drone/sensors/imu6050dmp.py
+++ drone/sensors/imu6050dmp.py
@@ -5,10 +5,12 @@ Created on 04/03/2016
 @author: david
 '''
 
+from copy import deepcopy
+import json
 import logging
 from math import degrees
+from os import path
 from threading import Thread, Lock
-from copy import deepcopy
 import time
 
 from sensors.pycomms.mpu6050 import MPU6050
@@ -20,6 +22,7 @@ class Imu6050Dmp(object):
     IMU-6050 using the DMP (digital motion processing) feature
     '''
 
+    CALIBRATION_FILE_PATH = "./sensor-calibration.json"
     GRAVITY = 9.807
 
     def __init__(self):
@@ -200,7 +203,7 @@ class Imu6050Dmp(object):
 
     
     def calibrate(self):
-        
+    
         print "Calibrating..."
         time.sleep(20)        
         self._imu.resetFIFO()
@@ -209,13 +212,29 @@ class Imu6050Dmp(object):
         time.sleep(0.05)
         
         with self._packetLock:
-            packet = deepcopy(self._packet) #self._readPacket()
+            packet = deepcopy(self._packet)
         
         q = self._imu.dmpGetQuaternion(packet)
         g = self._imu.dmpGetGravity(q)
         
-        ypr = self._imu.dmpGetYawPitchRoll(q, g)
-        self._angleOffset = [ypr["pitch"], ypr["roll"], ypr["yaw"]]
+        if path.exists(Imu6050Dmp.CALIBRATION_FILE_PATH):
+                        
+            with open(Imu6050Dmp.CALIBRATION_FILE_PATH, "r") as calibrationFile:
+                serializedCalibration = " ".join(calibrationFile.readlines())
+                calibrationFile.close()
+                
+            self._angleOffset = json.loads(serializedCalibration)
+            
+        else:
+                
+            ypr = self._imu.dmpGetYawPitchRoll(q, g)
+            self._angleOffset = [ypr["pitch"], ypr["roll"], ypr["yaw"]]
+            
+            #Save calibration
+            serializedCalibration = json.dumps(self._angleOffset)
+            with open(Imu6050Dmp.CALIBRATION_FILE_PATH, "w+") as calibrationFile:
+                calibrationFile.write(serializedCalibration + "\n")
+                calibrationFile.close()
         
         accelRaw = self._imu.dmpGetAccel(packet)
         linearAccel = self._imu.dmpGetLinearAccel(accelRaw, g)
